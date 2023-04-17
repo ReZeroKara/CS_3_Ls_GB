be7f04885e425ccafd75269482d6928c3bc71ff4
ReZero <ghostofpast@icloud.com>
2023-04-17 18:11:35 +0300

    add project002

---
+++ project002_SortArray/Program.cs
@@ -0,0 +1,37 @@
+﻿int [] arr ={ 1, 5, 8, 9, 3, 6, 2, 7, 3, 6, 10, 9 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[ ");
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
